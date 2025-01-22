669a3fca65578d01d9e8855ddf94b352980f6c1e
nja <naycro@hotmail.fr>
2025-01-22 20:03:43 +0100

    2. Fix 60 FPS

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,19 +1,20 @@
 import pygame
-
 from constants import *
+
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        screen.fill("black")
         pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
