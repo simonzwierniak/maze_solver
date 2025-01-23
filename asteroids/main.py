1f6a15eea973f62ab549d77611a4255e330be99e
nja <naycro@hotmail.fr>
2025-01-23 20:20:19 +0100

    3. Triangle moves

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-
+from player import Player
 
 
 def main():
@@ -8,13 +8,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        player.update(dt) #
+        screen.fill("black") # paint the screen in black
+        player.draw(screen) # draw the player's triangle
+        pygame.display.flip() # refresh the display window
+        dt = clock.tick(60) / 1000 # limit the framerate to  60 FPS 
+        
+        
 
 if __name__ == "__main__":
     main()
