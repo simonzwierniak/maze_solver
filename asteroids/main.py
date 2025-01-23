62c060312180078638f1d702ef40881bb5ab1fbc
nja <naycro@hotmail.fr>
2025-01-23 20:31:05 +0100

    4. Optimize game loop with groups

--- asteroids/main.py
+++ asteroids/main.py
@@ -2,24 +2,31 @@ import pygame
 from constants import *
 from player import Player
 
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt) #
+            
+        for obj in updatable: # iterate over all the updatable objects
+            obj.update(dt)
+
         screen.fill("black") # paint the screen in black
-        player.draw(screen) # draw the player's triangle
+        
+        for obj in drawable: # iterate over all the drawable objects
+            obj.draw(screen)
+
         pygame.display.flip() # refresh the display window
         dt = clock.tick(60) / 1000 # limit the framerate to  60 FPS 
-        
-        
+          
 
 if __name__ == "__main__":
     main()
